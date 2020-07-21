5cbdb7e3ca207d8fe9bdae5698f0e81360def0d7
Max <bloodmailvol1@gmail.com>
2020-07-21 21:30:26 +0300

    Подключил поддержку Views и Controllers
    А также подключил статические файлы

--- ASPCoreL2Services/ASPCoreL2Services/Startup.cs
+++ ASPCoreL2Services/ASPCoreL2Services/Startup.cs
@@ -7,18 +7,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPCoreL2Services
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,6 +26,8 @@ namespace ASPCoreL2Services
             }
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
