f456acf3c92f5e929ef781186ec592e81d49285f
Max <bloodmailvol1@gmail.com>
2020-07-25 19:41:44 +0300

    Добавил класс Config и подключил его в проект

---
+++ ASPCoreL2Services/ASPCoreL2Services/Service/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPCoreL2Services.Service
+{
+    public class Config
+    {
+        public string ConnectionString { get; set; }
+        public string SiteName { get; set; }
+    }
+}
--- ASPCoreL2Services/ASPCoreL2Services/Startup.cs
+++ ASPCoreL2Services/ASPCoreL2Services/Startup.cs
@@ -8,14 +8,20 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ASPCoreL2Services.Service;
 
 namespace ASPCoreL2Services
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+            Configuration.Bind("Project", new Config());
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
