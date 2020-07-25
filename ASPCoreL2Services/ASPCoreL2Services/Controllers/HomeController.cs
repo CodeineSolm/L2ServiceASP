a34cb26b7803fe00fded90ae5839e03273826c2c
Max <bloodmailvol1@gmail.com>
2020-07-26 01:14:41 +0300

    Изменил в мастер странице вызов RenderBody
    Соответсвенно изменил страницу Index, также добавил ServicePage и метод в HomeController

--- ASPCoreL2Services/ASPCoreL2Services/Controllers/HomeController.cs
+++ ASPCoreL2Services/ASPCoreL2Services/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ASPCoreL2Services.Domain;
 
 namespace ASPCoreL2Services.Controllers
 {
     public class HomeController : Controller
     {
+        private L2Context db = new L2Context();
         public IActionResult Index()
         {
-            return View();
+            var Items = db.DBEntityBase;
+            return View(Items);
+        }
+        public IActionResult ServicePage(int item_id)
+        {
+            var Item = db.DBEntityBase.FirstOrDefault(x => x.Id == item_id);
+            return View(Item);
         }
     }
 }
